5d4a90bbdefbe2cd11c022344d5582d931a3017d
rstbarth <rstbarth@gmail.com>
2026-03-31 18:03:36 -0300

    Add files via upload

--- js/store.js
+++ js/store.js
@@ -1,4 +1,4 @@
-window.SCOREPLACE_VERSION = '0.3.1-alpha';
+window.SCOREPLACE_VERSION = '0.3.2-alpha';
 
 // ─── Constantes globais ─────────────────────────────────────────────────────
 window.SCOREPLACE_URL = 'https://scoreplace.app';
