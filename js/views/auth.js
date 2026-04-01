6b8de258c887a90216044456d0f17b27f4b98a86
rstbarth <rstbarth@gmail.com>
2026-04-01 10:04:31 -0300

    Add files via upload

--- js/views/auth.js
+++ js/views/auth.js
@@ -696,7 +696,7 @@ async function simulateLoginSuccess(user) {
   // Set view mode to organizer
   window.AppStore.viewMode = 'organizer';
   var viewModeBtn = document.getElementById('view-mode-selector');
-  if (viewModeBtn) viewModeBtn.innerHTML = '<span style="font-size:1.2rem;">👁️</span><span style="font-size:0.7rem;font-weight:600;">Organizador</span>';
+  if (viewModeBtn) viewModeBtn.innerHTML = '👁️ <span style="font-weight:600;">Organizador</span>';
 
   // Update visibility of view mode selector
   if (typeof window.updateViewModeVisibility === 'function') {
